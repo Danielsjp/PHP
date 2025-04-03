ba62cfa0d9b82087ed4d9c5e4f585f52117f7c1f
daniel.sanchez <sjp.daniel@gmail.com>
2025-04-03 19:09:08 +0200

    hello

--- php_01/php_01.php
+++ php_01/php_01.php
@@ -1,5 +1,9 @@
 <?php
-
+/* comentario 
+multi 
+linea */
+// Comentario de una linea
+# Comentario de una linea 
 
 $entero = 1;
 $decimal = 1.2;
@@ -48,3 +52,44 @@ for ($i=0; $i< count($frutas); $i++)
     
     echo "<p> $frutas[$i]</p>";
 }
+
+foreach($nombreCompleto as $clave => $valor){
+
+    echo "<p> La clave $clave y el valor es $valor</p>";
+
+}
+
+$nombre = "Pepe"
+
+//ahora cerramos parte de php
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Soy el titulo</h1>
+    
+    <?php
+
+        if ($nombre == "Peter"){
+            echo  "<img src='1200px-Spider-Man.jpg' with='300' height='300'>";
+        }
+        elseif ($nombre == "Clarke") {
+            echo "<img src='images.jfif' with='300' height='300'>";
+        }
+        else {
+
+            echo "<img src='shinshan.jpg' with='300' height='300'>";
+        }
+
+    ?>
+
+</body>
+</html>
+
+
