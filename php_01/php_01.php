6e6a3532adab624b9c7d0833429786756dfe8ef7
daniel.sanchez <sjp.daniel@gmail.com>
2025-04-03 18:06:29 +0200

    hello

--- php_01/php_01.php
+++ php_01/php_01.php
@@ -4,6 +4,25 @@
 $entero = 1;
 $decimal = 1.2;
 $string = "El numero es $entero";
+$string2 = 'El numero es $entero';
+$frase = "She said me that \"I'm the best\"";
+echo $frase;
 
 echo $string;
+echo($decimal);
+ECHO($string);
+
+$nombre = "Peter";
+$apellido = "Parker";
+$nombreCompleto = "<p>".$nombre." ".$apellido."</p>";
+
+print($nombreCompleto);
+
+$frutas = ["kiwi", "pera", "mango"];
+
+echo $frutas[0];
+
+$nombreCompleto = ["nombre" => "Mike", "apellido" => "Corleone"];
+echo $nombreCompleto["apellido"];
+
 
